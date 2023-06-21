237f4fdfec305fc801f61d4b6a8e74dd7bd29f35
Jeff Zapotoczny <jeff.zapotoczny@sonarsource.com>
2023-06-21 14:23:15 -0500

    Adding some new code

---
+++ src/main/java/org/owasp/webgoat/lessons/vulnerablecomponents/DSAST.java
@@ -0,0 +1,43 @@
+package org.owasp.webgoat.lessons.vulnerablecomponents;
+
+import org.hsqldb.lib.FileUtil;
+import org.jsoup.helper.DataUtil;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+@RestController
+public class DSAST {
+    @PostMapping("/DAST/Spring/delete")
+    public void springDelete(@RequestParam String input) {
+
+        String directory = "./target/directory";
+        String[] filenames = StringUtils.commaDelimitedListToStringArray(input);
+        for (String filename: filenames) {
+            var file = new File(directory, filename);
+            FileSystemUtils.deleteRecursively(file);
+        }
+    }
+
+    @PostMapping("/DAST/hsqldb/delete")
+    public void hsqldbDelete(@RequestParam String filename) {
+
+        String directory = "./target/directory";
+        var file = new File(directory, filename);
+        FileUtil.getFileUtil().delete(filename);
+    }
+
+    public void jsoupLoad(@RequestParam String filename) throws IOException {
+
+        String directory = "./target/directory";
+        var file = new File(directory, filename);
+        DataUtil.load(file, "utf-8", "uri://");
+    }
+}
